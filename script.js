5889c632e24a8afa2783e1c6422499ac58a3b170
Nicholas <swaggychoz@gmail.com>
2022-09-01 16:38:43 +0800

    Fix getElementsByClass syntax and create a restart button

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+const checkGame = (function () {
+  const winningAxes = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [7,5,3]
+  ];
+
+  //cache DOM
+  const announcement = document.querySelector('.announcement')
+
+  function checkScoreOne () {
+    if (game.gameBoard.length >= 5) {
+      winningAxes.forEach(array => {
+        let totalPoints = [];
+        for (let i = 0; i < 3; i++) {
+          const variable = `${array[i]}`;
+          // console.log(variable);
+          const floor = document.getElementsByClassName(variable);
+          // console.log(floor[0].innerHTML);
+          if (floor[0].innerHTML == 'X') totalPoints.push('X');
+          else return;
+        };
+        
+        if (totalPoints.length == 3) {
+          createDiv('Player One Wins!');
+          createBtn('Restart', clearField);
+        } else return;
+      });
+    } else return;
+  };
+
+  function checkScoreTwo () {
+    if (game.gameBoard.length >= 6) {
+      winningAxes.forEach(array => {
+        let totalPoints = [];
+        for (let i = 0; i < 3; i++) {
+          const variable = `${array[i]}`;
+          const floor = document.getElementsByClassName(variable);
+          if (floor[0].textContent == 'O') totalPoints.push('O');
+          else return;
+        };
+        
+        if (totalPoints.length == 3) {
+          createDiv('Player Two Wins!');
+          createBtn('Restart', clearField);
+        } else return;
+      });
+    } else return;
+  };
+
+  function createDiv (message) {
+    const div = document.createElement('div');
+    div.textContent = message;
+    announcement.appendChild(div);
+  }
+
+  function clearField () {
+    gameBoard = [];
+    game.floors.forEach(floor => floor.innerText = '');
+    announcement.replaceChildren();
+  }
+
+  function createBtn(content, func) {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.textContent = content;
+    btn.addEventListener('click', func);
+    announcement.appendChild(btn);
+  }
+
+  return {
+    checkScoreOne,
+    checkScoreTwo
+  }
+
+})();
+
 const game = (function () {
   let gameBoard = [];
 
@@ -9,6 +91,8 @@ const game = (function () {
 
   floors.forEach(floor => {
     floor.addEventListener('click', addMark);
+    floor.addEventListener('click', checkGame.checkScoreOne);
+    floor.addEventListener('click', checkGame.checkScoreTwo)
   });
 
   function addMark() {
@@ -26,53 +110,8 @@ const game = (function () {
   };
 
   return {
-      gameBoard
+    gameBoard,
+    floors
   };
-
-})();
-
-const player = (name) => {
-  let playerName = name;
-
-}
-
-const checkGame = (function () {
-  const winningAxes = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [7,5,3]
-  ];
-
-  //cache DOM
-  const announcement = document.querySelector('.announcement')
-
-  winningAxes.forEach(array => {
-    let totalPoints = [];
-    for (let i = 0; i > 3; i++) {
-      const floor = document.getElementsByClassName(array[i]);
-      if (floor.innerText == 'X') total.push[null];
-      else return;
-    };
-    if (totalPoints.length == 3) {
-      console.log('Player One Wins!');
-    }
-  });
-
-  function createDiv (message) {
-    const div = document.createElement('div');
-    div.textContent = message;
-    announcement.appendChild(div);
-  }
-
-  function clearField () {
-    gameBoard = [];
-    floors.forEach(floor => floor.innerText = '');
-  }
-
 
 })();
